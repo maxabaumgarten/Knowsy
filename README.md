c542e628d7ee1f174e8069f89bce30693273a914
Max <maxabaum@gmail.com>
2020-11-22 21:27:02 -0500

    test code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Knowsy
-A python program that tells you everything you want to know about an IP address or Domain Name
+A python program that tells you everything you want to know about an IP address or Domain Name.
+
+# Status
+This is as pre-alpha as it gets.
---
+++ knowsy.py
@@ -0,0 +1,38 @@
+#W.I.P.
+
+import sys
+import os
+import platform
+import ipaddress
+
+
+
+#Ask user for IP address
+#format/validate input
+print("Welcome to Knowsy!  I know everything about IP addresses and Domains.")
+
+address = input("What IP address do you want to know about? " )
+
+while True:
+    try:
+        ipaddress.ip_address(address)
+    except ValueError:
+        print(f"'{address}' is not a valid IP address")
+        break
+    else:
+        #determine current operating system
+        current_os =  platform.system()
+        current_os = current_os.lower()
+        print(current_os)
+
+        #each os should be its own class
+        #if statement for classes based on OS
+        if current_os == 'windows':
+            dns_result = os.system('nslookup ' + address)
+            print(dns_result)
+
+        elif current_os == 'linux':
+            dns_result == os.system('dig ' + address)
+            print(dns_result)
+        break
+
