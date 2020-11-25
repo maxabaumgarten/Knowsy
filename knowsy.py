c6e9ba52ed44de9ca2e1cb76693fa58bad9da8a1
Max <maxabaum@gmail.com>
2020-11-25 15:44:13 -0500

    linux ping fix

--- knowsy.py
+++ knowsy.py
@@ -31,34 +31,3 @@ ip.ping_check(current_os.return_os())
 
 ip.set_ip_address("codingiscool")
 print(ip.return_ip_address())
-
-
-#old code
-""" while True:
-    try:
-        ipaddress.ip_address(address)
-    except ValueError:
-        print(f"'{address}' is not a valid IP address")
-        break
-    else:
-        #determine current operating system
-        current_os =  platform.system()
-        current_os = current_os.lower()
-        print(current_os)
-
-        #each os should be its own class
-        #if statement for classes based on OS
-        if current_os == 'windows':
-            dns_result = os.system('nslookup ' + address)
-            ping_result = os.system('ping ' + address)
-            trace_result = os.system('tracert ' + address)
-
-
-            print(dns_result)
-
-
-
-        elif current_os == 'linux':
-            dns_result = os.system('host ' + address)
-            print(dns_result)
-        break """
