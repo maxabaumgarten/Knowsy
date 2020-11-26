f9a5301ad18c8d53f2a9fc41d4e6c6452775066a
Max <maxabaum@gmail.com>
2020-11-25 23:34:54 -0500

    output updates

--- knowsy.py
+++ knowsy.py
@@ -5,11 +5,11 @@ import csv
 from address import IpAddress
 from operating_system import OperatingSystem
 
-print("Welcome to Knowsy!  I know everything about IP addresses and Domains(coming soon).")
+print(" ༼ つ ◕_◕ ༽つ Welcome to Knowsy!  I know everything about IP addresses and Domains(coming soon).")
 
 #Determine Operating System information
 #This may be removed as program is being developed as OS agnostic
-print(f"\nLet's check what operating system you are running.")
+print(f"\n ༼ つ ◕_◕ ༽つ Let's check what operating system you are running.")
 
 os_check =  platform.system()
 current_os = OperatingSystem(os_check)
@@ -26,7 +26,7 @@ testping = 'Yes'
 testroute = '10.0.1.1 > 6.7.8.9 > 9.9.9.1'
 
 #Create and Setup the CSV
-name_request = input("Save Knowsy CSV file as? ")
+name_request = input(" ༼ つ ◕_◕ ༽つ Save Knowsy CSV file as? ")
 filename = name_request + '.csv'
 
 with open(filename, 'w') as file_object:
@@ -34,8 +34,10 @@ with open(filename, 'w') as file_object:
     file_writer.writerow(['IP', 'Domain', 'Ping', 'Routes'])
 
 #Ask user for whole file
-hostfile = input("What is the name of your host file (.txt ONLY)? ")
+#TODO validate file exists, exception catch
+hostfile = input(" ༼ つ ◕_◕ ༽つ What is the name of your host file (.txt ONLY)? ")
 
+print("༼ つ ◕_◕ ༽つ Knowsy has begun the process of putting it's Knowse in things...")
 
 with open(hostfile, "r") as h_file:
     for host in h_file:
@@ -48,4 +50,8 @@ with open(hostfile, "r") as h_file:
                 file_writer = csv.writer(file_object, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 file_writer.writerow([ip.return_ip_address(), testdomain, ping_answer, testroute])
         else:
-            print(f"{ip.return_ip_address()} is not valid.")0
+            with open(filename, 'a') as file_object:
+                file_writer = csv.writer(file_object, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                file_writer.writerow([ip.return_ip_address(), 'Invalid IP', 'Invalid IP', 'Invalid IP'])
+
+print("༼ つ ◕_◕ ༽つ Knowsy Knows things about your network.  Your CSV is ready!")
